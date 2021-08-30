cb4c283d8e0af06b9f06b2d3cef1a2adaf9f7971
Elizabeth <A01412774@tec.mx>
2021-08-29 19:44:49 -0500

    termine ejercicio 1 de pendiente linea

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,15 @@
+import math
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    pal = int(input('Dame el número de palabras: '))
+    if pal <= 475:
+        costo1 = 60 - (60 * 0.10)
+        print('El costo de la publicación es:', costo1)
+    else:
+        if pal > 475:
+            num_pag = math.ceil(pal / 475)
+        costo2 = (num_pag * 60)  - ((num_pag * 60) * 0.10)
+        print('El costo de la publicación es:', costo2)
 
 if __name__ == '__main__':
     main()
--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,7 +1,8 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    gr = float(input('Dame la harina en gramos: '))
+    total = (gr * 50) / 1000
+    print('Necesitas estos gramos de levadura:', total)
 
 if __name__ == '__main__':
     main()
